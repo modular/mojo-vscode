142d5bae549f7bfc54d1da3ba58090e458635bc5
Lily Brown <lbrown@modular.com>
2025-07-07 11:02:12 -0700

    remove debug log

--- extension/sdk/magicSdk.ts
+++ extension/sdk/magicSdk.ts
@@ -177,7 +177,6 @@ async function getLatestNightlyMAXVersion(
   privateDir: string,
 ): Promise<Optional<string>> {
   const versions = await getAllNightlyMAXVersions(logger, privateDir);
-  logger.info('versions', versions);
   if (versions === undefined) {
     return undefined;
   }
