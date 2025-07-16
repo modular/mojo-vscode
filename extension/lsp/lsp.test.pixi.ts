2a2eda683bb9e6d3f06c12f11cf29b28969873b8
Lily Brown <lbrown@modular.com>
2025-07-16 15:28:59 -0700

    Improve testing support (#7)
    This will allow us to test SDK-related features again in the aftermath of #5.

--- extension/lsp/lsp.test.pixi.ts
+++ extension/lsp/lsp.test.pixi.ts
@@ -16,8 +16,8 @@ const repoConfig = {
   fixtures: path.join(__dirname, '..', '..', 'fixtures'),
 };
 
-suite('LSP', () => {
-  test('LSP should not be loaded on startup', async () => {
+suite('LSP', function () {
+  test('LSP should not be loaded on startup', async function () {
     // Restart the extension. Tests run in a shared environment, so if other tests
     // have created the LSP, this test will fail otherwise.
     await vscode.commands.executeCommand('mojo.extension.restart');
@@ -25,12 +25,16 @@ suite('LSP', () => {
     assert.strictEqual(extension.lspManager!.lspClient, undefined);
   });
 
-  test('LSP should be launched when a Mojo file is opened', async () => {
+  test('LSP should be launched when a Mojo file is opened', async function () {
+    // Restart the extension. Tests run in a shared environment, so if other tests
+    // have created the LSP, this test will fail otherwise.
+    await vscode.commands.executeCommand('mojo.extension.restart');
+
     const lsp = firstValueFrom(extension.lspManager!.lspClientChanges);
 
     await vscode.workspace.openTextDocument(
       vscode.Uri.file(
-        path.join(repoConfig.fixtures, 'dangling-file', 'dangling_file.mojo'),
+        path.join(repoConfig.fixtures, 'pixi-workspace', 'main.mojo'),
       ),
     );
 
